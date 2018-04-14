8e7bc7635632507b299e4749f0c093b1e76870ad
penglzh <771623810@qq.com>
2018-04-14 22:34:53 +0800

    first code commit

---
+++ HuobiBot/HuobiBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HuobiBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
